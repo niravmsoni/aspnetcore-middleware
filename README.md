658698fe55dd9cd325e00eaacd72c2680f5c9140
Nirav Soni <soninirav910@gmail.com>
2024-01-21 12:46:56 +0530

    testing middlewares

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Middleware
+
+	- Built-in middlewares
+		- ASP.NET Core ships with built-in middlewares
+		- Refer here for middleware sequence - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-8.0
+
+	- Custom middlewares
+		- We can create our own custom middlewares
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace Middleware
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Middleware
 {
     public class Startup
     {
@@ -14,10 +16,10 @@
 
             //Catches exception thrown in following middlewares
             //app.UseExceptionHandler();
-            
+
             //Adds Strict-Transport-Security header
             app.UseHsts();
-            
+
             //Redirects HTTP requests to HTTPS
             app.UseHttpsRedirection();
 
@@ -38,24 +40,100 @@
 
             //Custom middlewares go here
 
-            //Make sure request lands to correct Controller/Action
-            app.UseEndpoints(app => 
-            {
-                app.MapGet("/WeatherForecast", context =>
+            //Anonymous middleware
+            app.Use(
+                async (httpCtx, next) =>
                 {
-                    string[] Summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-                    var output = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-                    })
-                    .ToArray();
-
-                    context.Response.ContentType = "application/json";
-                    return context.Response.WriteAsJsonAsync(output);
+                    //Do some work with httpcontext.Request
+                    await next.Invoke();
+                    // Do something with httpContext.Response (Logging etc.)
                 });
+
+
+            //Use - Inserts middleware in pipeline. This usecase is of short-circuiting a request based on path-match
+            app.Use(
+                async (httpCtx, next) =>
+                {
+                    if (httpCtx.Request.Path.StartsWithSegments("/short-circuit"))
+                    {
+                        await httpCtx.Response.WriteAsync("Request terminated by short-circuit middleware.");
+                    }
+                    else
+                    {
+                        await next();
+                    }
+                });
+
+            //UseWhen - More focused on conditionally executing request based on HttpContext
+            app.UseWhen(context => context.Request.Path.StartsWithSegments("/use-when"),
+                branch =>
+                {
+                    branch.Use(async (context, next) =>
+                    {
+                        if (context.Request.Path.StartsWithSegments("/use-when"))
+                        {
+                            await context.Response.WriteAsync("Request terminated by use-when middleware");
+                        }
+                        else
+                        {
+                            await next();
+                        }
+                    });
+                });
+
+            //Map - More focused on conditionally executing middleware based on Request Path. - Need to troubleshoot this
+            app.Map("/map-test",
+                branch =>
+                {
+                    branch.Use(async (context, next) =>
+                    {
+                        if (context.Request.Path.StartsWithSegments("/map-test"))
+                        {
+                            await context.Response.WriteAsync("Request terminated by map middleware");
+                        }
+                        else
+                        {
+                            await next();
+                        }
+                    });
+                });
+
+            //Map more focused on Path.
+            app.MapWhen(context => context.Request.Path.StartsWithSegments("/map-when"),
+                branch =>
+                {
+                    branch.Use(async (context, next) =>
+                    {
+                        if (context.Request.Path.StartsWithSegments("/map-when"))
+                        {
+                            await context.Response.WriteAsync("Request terminated by map-when middleware");
+                        }
+                        else
+                        {
+                            await next();
+                        }
+                    });
+                });
+
+            //Enabling this will terminate all requests here if route is /ok or /internal-server-error
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Request terminates here");
             });
+
+            app.UseEndpoints(app =>
+                {
+                    app.MapGet("/ok", context =>
+                    {
+                        return context.Response.WriteAsync($"Request received: {context.Request.Path.Value}");
+                    });
+
+                    app.MapGet("/internal-server-error", context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        return context.Response.WriteAsync("Something went wrong");
+                    });
+                });
         }
     }
 }
